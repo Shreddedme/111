5a968d473a604e4db946b134424c0042272907bc
Shreddedme <10l.gl10@gmail.com>
2022-06-01 11:59:52 +0300

    111

--- 111/task1.php
+++ 111/task1.php
@@ -2,3 +2,18 @@
 $number = 442158755745;
 $int = 4;
 echo "В данном числе:"."<br />".substr_count($number, $int);
+
+
+
+
+$ch = 442158755745;
+$num = 4;
+$result = 0;
+settype($ch,'string');
+settype($num,'string');
+for($i=0;$i<=strlen($ch)-1;$i++){
+    if($ch[$i] == $num){
+        $result++;
+    }
+}
+echo 'Цифра '.$num.' в числе '.$ch.' встречается '.$result.' раза.';
