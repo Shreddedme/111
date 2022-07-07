e345a6000e38db45e25a9007ce0907f045a7a0f5
Shreddedme <10l.gl10@gmail.com>
2022-07-07 14:29:17 +0300

    Сравнить массивы

--- 111/Сравнить два массива.php
+++ 111/Сравнить два массива.php
@@ -1,20 +1,17 @@
 <?php
 
-function comp($a1, $a2) {
-    if(!isset($a1) or !isset($a2)) {
+function comp($a1, $a2)
+{
+    if (!isset($a1) or !isset($a2)) {
         return false;
     }
 
-        $sq = array_map('sqrt', $a2);
-        sort($a1);
-        sort($sq);
-    if ($a1 == $sq) {
-        return true;
-    }
-    else {
-    return false;
-    }
+    $sq = array_map('sqrt', $a2);
+    sort($a1);
+    sort($sq);
+    return $a1 == $sq;
 }
 
 $a1 = [121, 144, 19, 161, 19, 144, 19, 11];
 $a2 = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
+echo comp($a1, $a2);
