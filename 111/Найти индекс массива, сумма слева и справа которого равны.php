b849e80158b4fa41c9a05affb5cfbb2d27d9ba4c
Shreddedme <10l.gl10@gmail.com>
2022-07-07 09:34:14 +0300

    Если равны вернуть индекс, иначе -1

---
+++ 111/Найти индекс массива, сумма слева и справа которого равны.php
@@ -0,0 +1,17 @@
+<?php
+function find_even_index2($arr) {
+    foreach ($arr as $k=>$v) {
+        $leftArray = array_slice($arr, 0, $k);
+        $rightArray = array_slice($arr,$k + 1);
+
+        $leftSum = array_sum($leftArray);
+        $rightSum = array_sum($rightArray);
+        if ($leftSum == $rightSum) {
+            return $k;
+        }
+    }
+    return -1;
+}
+
+$arr = [1,2,3,4,3,2,1];
+echo  find_even_index2($arr);
