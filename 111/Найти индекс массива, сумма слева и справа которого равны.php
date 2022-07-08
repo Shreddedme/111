fa25e64e3270f04e4193b652a412b64c4f1cec7a
Shreddedme <10l.gl10@gmail.com>
2022-07-08 09:36:09 +0300

    Найти индекс массива, сумма слева и справа которого равны

--- 111/Найти индекс массива, сумма слева и справа которого равны.php
+++ 111/Найти индекс массива, сумма слева и справа которого равны.php
@@ -3,7 +3,6 @@ function find_even_index2($arr) {
     foreach ($arr as $k=>$v) {
         $leftArray = array_slice($arr, 0, $k);
         $rightArray = array_slice($arr,$k + 1);
-
         $leftSum = array_sum($leftArray);
         $rightSum = array_sum($rightArray);
         if ($leftSum == $rightSum) {
